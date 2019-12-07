82e54dd4ff5adc4cc32c0d2d6a26f79c8876083f
ravi-chandran <48181110+ravi-chandran@users.noreply.github.com>
2019-12-07 15:35:11 -0500

    Clean up comments

--- swbuilder/scripts/run_build.sh
+++ swbuilder/scripts/run_build.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 # maintainer: Ravi Chandran
-#
-# This file would typically invoke the build tool
-# which may invoke git to access repositories
-# requiring SSH keys, and perform the actual build
-# on the items in the /workdir folder.
-#
-# Here we'll simply 
-
-# uncomment for debug
-#echo "argc = ${#*}"
-#echo "argv = ${*}"
 
 cd /workdir
 
