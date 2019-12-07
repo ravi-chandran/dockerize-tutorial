07fbb075a07b8f336b12719ed0ccd22e20edab8f
ravi-chandran <48181110+ravi-chandran@users.noreply.github.com>
2019-12-07 16:39:16 -0500

    Clean up code

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # maintainer: Ravi Chandran
 
-docker container run                                       \
-    --volume $(pwd)/swbuilder/scripts:/scripts             \
-    --volume $1:/workdir                                   \
-    --user $(id -u ${USER}):$(id -g ${USER})               \
-    --rm -it --name build_swbuilder swbuilder:v1       \
+docker container run                              \
+    --volume $(pwd)/swbuilder/scripts:/scripts    \
+    --volume $1:/workdir                          \
+    --user $(id -u ${USER}):$(id -g ${USER})      \
+    --rm -it --name build_swbuilder swbuilder:v1  \
     build
